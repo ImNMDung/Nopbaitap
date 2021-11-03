31ebf6726bd75f4687363ff10f0f152bd4dfedfd
eovip <eovip@192.168.1.6>
2021-11-03 14:50:20 +0700

    Nhập Xuất Dữ Liệu Bằng File

---
+++ src/Nhapxuatdulieu.java
@@ -0,0 +1,42 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Scanner;
+public class Nhapxuatdulieu {
+	public static void main(String[] args) { 
+		  try {
+		   //Bước 1: Tạo đối tượng luồng và liên kết nguồn dữ liệu
+		   FileOutputStream fos = new FileOutputStream("f:/filename.txt");
+		   DataOutputStream dos = new DataOutputStream(fos);
+		   //Bước 2: Ghi dữ liệu
+		   String n;
+	        Scanner scaner = new Scanner(System.in);
+	        n = scaner.nextLine();
+		   dos.writeBytes(n);  
+		   //Bước 3: Đóng luồng
+		   fos.close();
+		   dos.close();
+		   System.out.println("Done!");
+		   
+		
+		  
+		  FileInputStream fis = new FileInputStream("f:/filename.txt");
+		   DataInputStream dis = new DataInputStream(fis);
+		   //Bước 2: Đọc dữ liệu
+		   String m = dis.readLine();
+		   //Bước 3: Đóng luồng
+		   fis.close();
+		   dis.close();
+		   //Hiển thị nội dung đọc từ file
+		   System.out.println( m);
+		  
+		  } catch (IOException ex) {
+		    ex.printStackTrace();
+	}
+		
+		 
+}}
+
+		
