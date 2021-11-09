02d1a25c47c59a398e33dc65b4190474a789d2c5
eovip <eovip@192.168.1.4>
2021-11-09 20:06:18 +0700

    BaiTapThuaKe

---
+++ src/ThuaKe/Bai1/Person.java
@@ -0,0 +1,49 @@
+package ThuaKe.Bai1;
+
+public class Person {
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public char getGender() {
+		return gender;
+	}
+
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	public int getBirthYear() {
+		return birthYear;
+	}
+
+	public void setBirthYear(int birthYear) {
+		this.birthYear = birthYear;
+	}
+
+	@Override
+	public String toString() {
+		return "Name: " + name + ",\n Gender: " + gender + ",\n BirthYear: " + birthYear;
+	}
+
+	String name;
+	char gender;
+	int birthYear;
+	
+	public Person() {
+		super();
+	}
+
+	public Person(String name, char gender, int birthYear) {
+		super();
+		this.name = name;
+		this.gender = gender;
+		this.birthYear = birthYear;
+	}
+	
+	
+}
---
+++ src/ThuaKe/Bai1/Student.java
@@ -0,0 +1,80 @@
+package ThuaKe.Bai1;
+
+import java.util.Scanner;
+
+public class Student extends Person {
+	@Override
+	public String toString() {
+		return "Student :\n studentID: " + studentID + " ,\n Name: "+ name + ",\n AcademicYear: " + academicYear;
+	}
+	
+	public Student() {
+		super();
+	}
+
+	public Student(String studentID, int academicYear) {
+		super();
+		this.studentID = studentID;
+		this.academicYear = academicYear;
+	}
+	
+	
+	
+	public Student(String name, char gender, int birthYear, String studentID, int academicYear) {
+		super(name, gender, birthYear);
+		this.academicYear = academicYear;
+		this.studentID = studentID;
+		
+		
+	}
+
+	public String getStudentID() {
+		return studentID;
+	}
+	public void setStudentID(String studentID) {
+		this.studentID = studentID;
+	}
+	public int getAcademicYear() {
+		return academicYear;
+	}
+	public void setAcademicYear(int academicYear) {
+		this.academicYear = academicYear;
+	}
+	
+	String studentID;
+	int academicYear;
+	
+	public void setInfo() {
+		Scanner input = new Scanner(System.in);
+		System.out.print("Nhap ho ten: ");
+		this.setName(input.next());
+
+		System.out.print("Nhap ma sinh vien: ");
+		this.setStudentID(input.next());
+		
+		System.out.print("Nhap nam sinh: ");
+		this.setBirthYear(input.nextInt());
+		
+		System.out.print("Nhap gioi tinh: ");
+		this.setGender(input.next().charAt(0));
+		
+		System.out.print("Nhap nam nhap hoc: ");
+		this.setAcademicYear(input.nextInt());
+		
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
